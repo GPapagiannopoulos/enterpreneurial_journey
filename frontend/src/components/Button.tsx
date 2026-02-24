64115c9bc342b94a426d6e4e322d7027dbb34fc9
gp925 <gp925@id.ac.uk>
2026-02-24 18:46:50 +0000

    feat(home-page): implement skeleton for home page

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,13 @@
+interface ButtonProps {
+    label: string,
+    onClick: () => void,
+    variant?: 'primary' | 'secondary'
+}
+
+export default function Button({label, onClick, variant = 'primary'}: ButtonProps) {
+    return (
+        <button className={`btn-${variant}`} onClick={onClick}>
+            {label}
+        </button>
+    );
+}
