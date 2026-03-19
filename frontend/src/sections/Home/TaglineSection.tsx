55fa3f56980609ce8e9707b86c72845405db8058
gp925 <gp925@id.ac.uk>
2026-03-19 23:13:30 +0000

    feat: implement tagline on landing page

--- frontend/src/sections/Home/TaglineSection.tsx
+++ frontend/src/sections/Home/TaglineSection.tsx
@@ -1,3 +1,16 @@
 export default function TaglineSection() {
-  return <section>TaglineSection</section>;
+  return (
+    <section className="py-12 bg-gray-50">
+      <div className="max-w-3xl mx-auto px-8 text-center">
+        <h2 className="text-4xl font-extrabold text-gray-900 mb-2 leading-loose">
+          Not Just Another <br />
+          <span className="py-10 text-green-600">Investment Platform</span>
+        </h2>
+        <p className="text-gray-500 text-lg">
+          We ditched the corporate speak <br />
+          Here's what actually matters <br />
+        </p>
+      </div>
+    </section>
+  );
 }
