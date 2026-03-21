3ccc8e48a68d01884b572c55fd75654548cb8db5
gp925 <gp925@id.ac.uk>
2026-03-21 23:34:37 +0000

    feat: implement complete FAQ functionality

--- frontend/src/sections/ForDevelopers/DeveloperFAQs.tsx
+++ frontend/src/sections/ForDevelopers/DeveloperFAQs.tsx
@@ -35,16 +35,24 @@ export default function DeveloperFAQs() {
   return (
     <section>
       {FAQs.map((FAQ, index) => (
-        <div key={index}>
-          <div>
-            <h2>
-              {index}. {FAQ.question}
-            </h2>
-            <button onClick={() => toggleFAQ(index)}>
+        <div
+          key={index}
+          className="border-gray-400 border-b-2 py-4 max-w-4xl mx-auto mb-8"
+        >
+          <div className="flex justify-between items-center px-14">
+            <h2 className="font-bold text-lg">{FAQ.question}</h2>
+            <button
+              onClick={() => toggleFAQ(index)}
+              className="text-3xl text-gray-500 font-light ease-in-out duration-300"
+            >
               {expandedFAQ === index ? "-" : "+"}
             </button>
           </div>
-          <div>{index === expandedFAQ ? <p>{FAQ.answer}</p> : null}</div>
+          <div
+            className={`overflow-hidden transition-all duration-300 ease-in-out ${expandedFAQ === index ? "max-h-96" : "max-h-0"} px-14`}
+          >
+            {FAQ.answer}
+          </div>
         </div>
       ))}
     </section>
