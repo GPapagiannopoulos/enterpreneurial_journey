f32eeedfd75a667a665a96567737bc562a82feb6
gp925 <gp925@id.ac.uk>
2026-03-22 10:06:58 +0000

    feat: implement Eligible project categories on For Developers page

--- frontend/src/sections/ForDevelopers/EligibleProjects.tsx
+++ frontend/src/sections/ForDevelopers/EligibleProjects.tsx
@@ -1,3 +1,52 @@
+const EnergyTypes: string[] = [
+  "Solar PV",
+  "Onshore Wind",
+  "Offshore Wind",
+  "BESS",
+  "Hydro",
+  "Biomass",
+];
+
+const DevelopmentStages: string[] = [
+  "Development",
+  "Ready-to-build",
+  "Construction",
+  "Operational",
+];
+
 export default function EligibleProjects() {
-  return <section></section>;
+  return (
+    <section className="px-14 mt-14">
+      <h2 className="font-bold text-3xl">Eligible Projects</h2>
+      <p className="font-light text-sm mb-4">
+        We are here for you regardless of the energy type or development stage
+      </p>
+      <div className="grid grid-cols-2 gap-8 ">
+        <div className="border border-gray-200 shadow-2xs rounded-2xl p-4">
+          <h3 className="font-bold">Technology Types</h3>
+          <div className="flex flex-wrap gap-2">
+            {EnergyTypes.map((type) => (
+              <div className="flex items-center gap-2 m-4">
+                <span className="border border-gray-200 rounded-full text-sm px-3 py-1">
+                  {type}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="border border-gray-200 shadow-2xs rounded-2xl p-4">
+          <h3 className="font-bold">Development Stages</h3>
+          <div className="flex flex-wrap gap-2">
+            {DevelopmentStages.map((stage) => (
+              <div className="flex items-center gap-2 m-4">
+                <span className="border border-gray-200 rounded-full text-sm px-3 py-1">
+                  {stage}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
