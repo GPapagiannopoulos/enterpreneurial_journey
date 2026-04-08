bed133cd7dcf9be0ab87680316628cc22460d380
gp925 <gp925@id.ac.uk>
2026-04-08 01:06:53 +0100

    feat: revamp how it works section

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import Navbar from "../components/Navbar";
 import HeroSection from "../sections/Home/HeroSection";
 import TaglineSection from "../sections/Home/TaglineSection";
-import FeaturesSection from "../sections/Home/FeatureSection";
-import StatsSection from "../sections/Home/StatsSection";
 import HowItWorksSection from "../sections/Home/HowItWorksSection";
 import ForEveryInvestorSection from "../sections/Home/ForEveryInvestorSection";
 import CTASection from "../sections/Home/CTASection";
@@ -13,8 +11,6 @@ export default function Home() {
       <Navbar />
       <HeroSection />
       <TaglineSection />
-      <FeaturesSection />
-      <StatsSection />
       <HowItWorksSection />
       <ForEveryInvestorSection />
       <CTASection />
