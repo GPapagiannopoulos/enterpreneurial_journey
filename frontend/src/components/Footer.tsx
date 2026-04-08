a04f88bb765f0798ea8f1887ca9748a9391f32ec
gp925 <gp925@id.ac.uk>
2026-04-08 21:42:42 +0100

    feat: update about us page

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,10 +5,10 @@ export default function Footer() {
     <section className="flex flex-col gap-9">
       <div className="flex justify-between pt-35 pb-10 gap-50 border-b border-black">
         <div className="flex flex-col gap-7">
-          <div className="text-4xl font-bold font-raleway">Novatech</div>
+          <div className="text-4xl font-bold font-raleway">Noverta</div>
           <div className="text-black text-xl font-raleway">
-            Novatech is a London-based startup bringing green energy
-            crowdfunding to the UK for investors and green developers
+            Noverta is a London-based startup bringing green energy crowdfunding
+            to the UK for investors and green developers
           </div>
         </div>
         <div className="grid grid-cols-3 gap-x-30 gap-y-15 text-black text-xl font-raleway">
