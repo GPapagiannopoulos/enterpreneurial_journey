17b99c51479bea87bee26dafad6348af4231a892
gp925 <gp925@id.ac.uk>
2026-04-08 18:37:51 +0100

    feat: route About Us endpoint

---
+++ frontend/src/pages/AboutUs.tsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function AboutUs() {
+  return (
+    <div className="min-h-screen px-35">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
