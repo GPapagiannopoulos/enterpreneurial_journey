c387017c447e5598c8283e60343cfe448f24e2e1
gp925 <gp925@id.ac.uk>
2026-05-16 13:37:17 +0100

    feat: implement commitment page

--- frontend/src/sections/AboutUs/Commitments.tsx
+++ frontend/src/sections/AboutUs/Commitments.tsx
@@ -1,28 +1,26 @@
-import { useNavigate } from "react-router-dom";
-import Button from "../../components/Button";
+import SolarPanel from "/src/assets/icons/SolarPanelsAboutUs.png";
 
 export default function Commitments() {
-  const navigate = useNavigate();
-
   return (
-    <section className="flex justify-between gap-25 pt-60 pb-26 border-b border-neutral-400">
-      <div className="text-black text-8xl font-bold font-raleway">
-        We make <span className="text-green-700">green</span> investing
-        accessible to everyone
+    <section className="px-30 flex flex-col gap-12">
+      <div className="text-teal-950 text-8xl font-bold font-raleway">
+        Commitments
       </div>
-      <div className="flex flex-col justify-center gap-17">
-        <div className="text-neutral-500 text-2xl font-inter">
-          We connect individuals and institutions with verified green projects
-          around the world, from solar farms in sub-Saharan Africa to
-          reforestation efforts in Southeast Asia. Our platforms makes it simple
-          to put your money where the planet needs it the most.
-        </div>
-        <div>
-          <Button
-            label="Book a consultation"
-            variant="primary"
-            onClick={() => navigate("/soon")}
-          ></Button>
+      <div className="grid grid-cols-[2fr_1fr] justify-center gap-12 border-[#DCD3D3] border-2">
+        <img src={SolarPanel} alt="Solar Panels" />
+        <div className="flex flex-col justify-center gap-10 text-2xl font-medium font-['DM_sans'] pr-15">
+          <h3 className="text-teal-950 text-6xl font-bold tracking-wide leading-12">
+            Noverta’s commitment is in our Nature
+          </h3>
+          <p className="text-[#063B27A8] leading-10 tracking-wide">
+            We support projects that contribute to a sustainable model
+          </p>
+          <p className="font-inter leading-10 tracking-tight">
+            We connect people and institutions with verified green projects
+            worldwide, making it easy to invest where the planet needs it most.
+            We connect people and institutions with verified green projects
+            worldwide.
+          </p>
         </div>
       </div>
     </section>
