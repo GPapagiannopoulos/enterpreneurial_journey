cb7777692bc0fef60163459c9647b5f932e423b4
gp925 <gp925@id.ac.uk>
2026-06-05 23:35:34 +0100

    feat: implement education hero

--- frontend/src/pages/Education/Education.mobile.tsx
+++ frontend/src/pages/Education/Education.mobile.tsx
@@ -1,3 +1,13 @@
+import Footer from "../../components/layout/Mobile/Footer";
+import Navbar from "../../components/layout/Mobile/Navbar";
+import Hero from "../../sections/Education/Mobile/Hero";
+
 export default function Education() {
-  return <section></section>;
+  return (
+    <section>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </section>
+  );
 }
